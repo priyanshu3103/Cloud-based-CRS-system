af21f1bff9a328064ccfda9a45fefee2d1ea6714
priyanshu3103 <priyanshubarapatre19@gmail.com>
2025-05-18 21:38:27 +0530

    initial

--- crs_reservation/src/main/java/com/crshotel/crs_reservation/CrsReservationApplication.java
+++ crs_reservation/src/main/java/com/crshotel/crs_reservation/CrsReservationApplication.java
@@ -2,9 +2,17 @@ package com.crshotel.crs_reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CrsReservationApplication {
+public class CrsReservationApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CrsReservationApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CrsReservationApplication.class, args);
     }
